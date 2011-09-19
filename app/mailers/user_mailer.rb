4d9ef1468ebcb3593faeecea63cbb6f7cfc4e23b
Andrea <andrea.damico@gmail.com>
2011-09-19 13:30:30 +0200

    adding mailer for sorcery

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "notifications@example.com"
+
+  def reset_password_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
+    mail(:to => user.email,
+         :subject => "Your password reset request")
+  end
+end
