45be07cffeeca6683a7b717efcc45210a0bbb33c
Andrea D'Amico <andrea.damico@gmail.com>
2011-11-25 11:17:51 +0100

    code organisation for dossier model

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1,21 +1,28 @@
 class Dossier < ActiveRecord::Base
+  #accessible attributes
   attr_accessible :name, :date_appel, :centre_id, :user_id
 
   extend FriendlyId
   friendly_id :code
 
+  # validations
   validates_presence_of :name, :date_appel, :centre_id, :user_id
+
+  #associations
   belongs_to :centre
   belongs_to :user
 
+  #delegations
   delegate :name, :code, :to => :centre, :prefix => true
   delegate :username, :to => :user, :allow_nil => true
 
+  #callbacks
   after_create :assign_code
   before_update do
     self.code = create_code
   end
 
+  #custom methods
   def year
     date_appel.beginning_of_year.year.to_s
   end
