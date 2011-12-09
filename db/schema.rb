f395a2bc6c31b738de63a5b9f8fde4f9a53a9a09
Andrea D'Amico <andrea.damico@gmail.com>
2011-12-09 10:43:30 +0100

    adding pg, thin, fixing db stuff for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(:version => 20111128160120) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
