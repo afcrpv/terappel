eaff22eee27980386dc1b16f455221a8c707c6be
Andrea D'Amico <andrea.damico@gmail.com>
2011-12-12 17:15:17 +0100

    adding ie css to precompile

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Terappel
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile << "ie*.css"
   end
 end
