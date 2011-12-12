7dfc4bacdcd67f30b565e21447b4f357ee4369ce
Andrea D'Amico <andrea.damico@gmail.com>
2011-12-12 11:19:19 +0100

    fixing cukes features

--- features/manage_expositions.feature
+++ features/manage_expositions.feature
@@ -14,7 +14,14 @@ Feature: Manage expositions
     When I add a new exposition for a dossier
     Then the added exposition should appear in the summary table
 
-  @javascript @focus
+  @javascript
   Scenario: update row in expo summary
     When I update an existing exposition for a dossier
     Then the corresponding row in the summary table should be updated
+
+  @javascript @focus
+  Scenario: update an existing row after adding a new row
+    When I add a new exposition for a dossier
+    And I add another exposition
+    And I update the first exposition
+    Then the first row should be updated
--- features/step_definitions/expositions_steps.rb
+++ features/step_definitions/expositions_steps.rb
@@ -28,10 +28,34 @@ end
 
 When /^I update an existing exposition for a dossier$/ do
   step %{I add a new exposition for a dossier}
+  click_on "M"
   fill_in "Posologie", :with => "50 mg/j"
   click_on "Valider"
 end
 
 Then /^the corresponding row in the summary table should be updated$/ do
   find(:css, '#expositions_summary tbody').should have_content('50 mg/j')
+end
+
+When /^I add another exposition$/ do
+  click_on "Ajouter Expo"
+  select "ACICLOVIR", :from => "Exposition"
+  select "T1", :from => "Terme d'exposition"
+  select "HERPES", :from => "Indication"
+  fill_in "Posologie", :with => "3 g/j"
+  fill_in "De", :with => "12"
+  fill_in "à", :with => "30"
+  click_on "Valider"
+end
+
+When /^I update the first exposition$/ do
+  within(:css, "table#expositions_summary tr:first-child td") do
+    click_on "M"
+  end
+  fill_in "Posologie", :with => "50 mg/j"
+  click_on "Valider"
+end
+
+Then /^the first row should be updated$/ do
+  find(:css, '#expositions_summary tbody tr:first-child').should have_content('50 mg/j')
 end
