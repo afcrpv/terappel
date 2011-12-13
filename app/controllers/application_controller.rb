e7088f0e19a006170709419246c784320036016d
Andrea D'Amico <andrea.damico@gmail.com>
2011-12-13 12:12:55 +0100

    various fixes to css to use twitter bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def redirect_with_flash(resource, path=nil, message=nil)
+  def redirect_with_flash(resource, flash_type=:success, path=nil, message=nil)
     path = resource unless path
     resource = resource[1] if resource.class == Array
     message = flash_message(resource) unless message
-    redirect_to path, :notice => message
+    redirect_to path, flash: {"#{flash_type}" => message}
   end
 
   private
