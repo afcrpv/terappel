b422daf49254754a109e03283d3eb4f1851c34d8
Andrea D'Amico <andrea.damico@gmail.com>
2013-05-12 11:44:54 +0200

    removing init for pry, rails admin, rails footnotes

--- config/initializers/rails_footnotes.rb
+++
@@ -1,6 +0,0 @@
-if defined?(Footnotes) && Rails.env.development?
-  Footnotes.run! # first of all
-
-  # ... other init code
-  Footnotes::Filter.prefix = 'mvim://open?url=file://%s&line=%d&column=%d'
-end
