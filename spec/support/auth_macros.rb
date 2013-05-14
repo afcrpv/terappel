98fe3e78e1362a41bd92afceb34b25d3261b3069
Andrea D'Amico <andrea.damico@gmail.com>
2013-05-14 17:44:28 +0200

    moving users authentication specs to rspec

--- spec/support/auth_macros.rb
+++ spec/support/auth_macros.rb
@@ -1,7 +1,8 @@
+include Warden::Test::Helpers
 module AuthMacros
   def login(user = nil)
     visit login_path
-    fill_in "Email", with: user.email
+    fill_in "Nom d'utilisateur", with: user.username
     fill_in "Mot de passe", with: user.password
     click_on "Connexion"
   end
