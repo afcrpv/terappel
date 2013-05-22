8bf9513109d0366ca2b4d3b1a3259147ddd4c61b
Andrea D'Amico <andrea.damico@gmail.com>
2013-05-22 17:40:15 +0200

    specs for correspondant on the fly edit

--- spec/features/dossiers_saisie_spec.rb
+++ spec/features/dossiers_saisie_spec.rb
@@ -6,6 +6,7 @@ feature "Dossiers saisie" do
   background do
     login user
     create(:dossier, code: "LY1111002", centre: user.centre)
+    create(:specialite, name: "spec1")
   end
 
   context "dossier global search" do
@@ -25,12 +26,38 @@ feature "Dossiers saisie" do
     end
   end
   context "correspondants" do
-    scenario "list for select2 is scoped by current user centre", focus: true do
+    scenario "list for select2 is scoped by current user centre" do
       corr = create(:correspondant, centre: user.centre)
       other_corr = create(:correspondant, centre: create(:centre))
       visit correspondants_url(format: :json)
       page.should have_content corr.fullname
       page.should_not have_content other_corr.fullname
+    end
+    scenario "can be created/updated from dossier form", js: true, focus: true do
+      visit new_dossier_path
+      click_link "Ajout"
+      sleep 2
+      page.should have_content "Nouveau correspondant"
+      fill_in "correspondant_nom", with: "test test"
+      select "spec1", from: "Spécialité"
+      fill_in "Ville", with: "Lyon"
+      fill_in "Code Postal", with: "69005"
+      within ".modal-footer" do
+        click_link "Enregistrer"
+      end
+      sleep 2
+      page.should have_content "test test - spec1 - 69005 - Lyon"
+      click_link "Voir/modifier"
+      sleep 2
+      within ".modal" do
+        page.should have_content "test test - spec1 - 69005 - Lyon"
+      end
+      fill_in "correspondant_nom", with: "test1 test"
+      within ".modal-footer" do
+        click_link "Enregistrer"
+      end
+      sleep 2
+      page.should have_content "test1 test - spec1 - 69005 - Lyon"
     end
   end
 end
