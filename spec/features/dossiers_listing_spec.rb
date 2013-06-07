34d02c223f1d51f01ce26e409f542a9820b85783
Andrea D'Amico <andrea.damico@gmail.com>
2013-06-07 13:38:22 +0200

    refactoring dossiers listing specs

--- spec/features/dossiers_listing_spec.rb
+++ spec/features/dossiers_listing_spec.rb
@@ -2,25 +2,25 @@ require "spec_helper"
 
 feature "Dossiers listing" do
   given(:user)          {create(:member)}
-  given(:dossier)       {create(:dossier, code: "LY1111002", centre: user.centre)}
+  given(:dossier)       {Dossier.first}
 
   background do
     login user
-    create(:specialite, name: "spec1")
+    create(:dossier, code: "LY2013001", centre: user.centre)
   end
 
   scenario "each row has actions for print/show and edit dossier", js: true, slow: true do
     visit dossiers_path
-    click_link "Imprimer le dossier LY1111002"
-    find("#modal_label").should have_content "Aperçu du dossier LY1111002"
+    click_link "Imprimer le dossier LY2013001"
+    find("#modal_label").should have_content "Aperçu du dossier LY2013001"
     click_button "Fermer"
-    click_link "Modifier le dossier LY1111002"
-    page.should have_content "Modification Dossier LY1111002"
+    click_link "Modifier le dossier LY2013001"
+    page.should have_content "Modification Dossier LY2013001"
   end
 
   scenario "export dossiers to excel" do
     visit dossiers_path(format: :xls)
-    page.should have_content("LY1111002")
+    page.should have_content("LY2013001")
   end
 
   scenario "export dossiers to pdf" do
@@ -28,17 +28,16 @@ feature "Dossiers listing" do
     page.response_headers["Content-Type"].should == "application/pdf"
   end
 
-  scenario "access dossier pdf", focus: true do
+  scenario "access dossier pdf" do
     visit dossier_path(dossier, format: :pdf)
     page.response_headers["Content-Type"].should == "application/pdf"
   end
 
   scenario "access dossier fiche dense", js: true do
-    dossier = create(:dossier, code_bnpv: "LY2013001", crpv: crpv1, enquete: enquete)
     visit dossiers_path
     click_link "Imprimer le dossier"
     within ".modal" do
-      page.should have_content "Fiche dossier LY1111002"
+      page.should have_content "Fiche dossier LY2013001"
     end
   end
 end
