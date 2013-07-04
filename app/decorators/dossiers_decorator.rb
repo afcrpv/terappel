d68d466310fd3b7e39b1fb0d486aace0eab5a2d6
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-04 11:59:12 +0200

    more columns for csv

--- app/decorators/dossiers_decorator.rb
+++ app/decorators/dossiers_decorator.rb
@@ -1,7 +1,12 @@
 require "csv"
 class DossiersDecorator < Draper::CollectionDecorator
-  COLUMNS_FOR_CSV = [:code, :date_appel, :motif_code, :age, :grsant, :fcs, :geu, :miu, :ivg, :img, :nai, :tabac, :alcool, :age_grossesse, :produit1, :produit2, :produit3, :indication, :dose, :expo_terme, :de, :a, :de2, :a2, :evolution, :terme, :modaccouch, :nbr_bebes, :sexe, :poids, :taille, :pc, :apgar1, :apgar5, :malformation, :pathologie, :a_relancer]
+  COLUMNS_FOR_CSV = [:code, :date_appel, :motif_code, :age, :grsant, :fcs, :geu, :miu, :ivg, :img, :nai, :tabac, :alcool, :age_grossesse, :evolution, :terme, :modaccouch, :nbr_bebes, :sexe, :poids, :taille, :pc, :apgar1, :apgar5, :malformation, :pathologie, :a_relancer]
 
+  (1..3).each do |i|
+    %w(produit indication posologie terme_expo).each do |name|
+        COLUMNS_FOR_CSV << :"#{name}#{i}"
+    end
+  end
   def to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << COLUMNS_FOR_CSV.map {|column| object.human_attribute_name(column) }
