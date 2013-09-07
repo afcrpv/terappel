5c8d7aaf82a96430b72d685c516f0376d5221f7f
Andrea D'Amico <andrea.damico@gmail.com>
2013-09-07 11:34:06 +0200

    old unused controller

--- app/controllers/authorized_controller.rb
+++
@@ -1,9 +0,0 @@
-class AuthorizedController < ApplicationController
-  before_filter :authenticate_user!
-  check_authorization # This is for CanCan to ensure authorization is checked for any controller action
-
-  rescue_from CanCan::AccessDenied do |exception|
-    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
-    redirect_to root_url, :alert => exception.message
-  end
-end
