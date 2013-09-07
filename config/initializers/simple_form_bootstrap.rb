c6389f5de650aa8242a48e854cacb6b3d8e532ab
Andrea D'Amico <andrea.damico@gmail.com>
2013-09-07 11:30:36 +0200

    bootstrap 3 initializer customization

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,28 +1,65 @@
-# Use this setup block to configure all options available in SimpleForm.
-SimpleForm.setup do |config|
-  config.input_class = "form-control"
+# http://stackoverflow.com/questions/14972253/simpleform-default-input-class
+# https://github.com/plataformatec/simple_form/issues/316
 
-  config.wrappers :bootstrap, tag: 'div', class: 'form-group', error_class: 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.use :input
-    b.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  FileInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+]
+
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.push('form-control')
+    end
   end
 
-  config.wrappers :group, tag: 'div', class: "form-group", error_class: 'error' do |b|
+  Object.const_set(input_type, new_class)
+end
+
+SimpleForm.setup do |config|
+  config.boolean_style = :nested
+
+  config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '} } do |b|
+
     b.use :html5
+    b.use :min_max
+    b.use :maxlength
     b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label_input
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+  config.wrappers :group, tag: 'div', class: "form-group", error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '} }  do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
     b.use :label
     b.use :input, wrap_with: { class: "input-group" }
     b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
-    b.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
   end
 
-  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
-  # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
-  # to learn about the different styles for forms and inputs,
-  # buttons and other elements.
-  config.default_wrapper = :bootstrap
+  config.default_wrapper = :bootstrap3
 end
