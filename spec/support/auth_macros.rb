f63e316ce6284d9ef55c1c89ae56548543e5b429
Andrea D'Amico <andrea.damico@gmail.com>
2014-02-14 13:45:08 +0100

    abstract auth macro

--- spec/support/auth_macros.rb
+++ spec/support/auth_macros.rb
@@ -2,8 +2,8 @@ include Warden::Test::Helpers
 module AuthMacros
   def login(user = nil)
     visit login_path
-    fill_in "Nom d'utilisateur", with: user.username
-    fill_in "Mot de passe", with: user.password
-    click_on "Connexion"
+    fill_in "user_username", with: user.username
+    fill_in "user_password", with: user.password
+    click_on "Se connecter"
   end
 end
