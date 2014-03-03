a3e1dfc7a0642715979d3b8f283841a30e7d804d
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-03 11:28:20 +0100

    basic atc/dci interface for product

--- app/models/atc.rb
+++ app/models/atc.rb
@@ -1,4 +1,8 @@
 class Atc < ActiveRecord::Base
   has_many :produits, through: :classifications
   has_many :classifications, dependent: :destroy
+
+  def to_s
+    [libabr, libelle].join(" - ")
+  end
 end
