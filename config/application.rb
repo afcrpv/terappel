3996cbcc0690478908e2ab537040bff5d2c2ad02
Andrea D'Amico <andrea.damico@gmail.com>
2014-11-13 11:32:47 +0100

    update config files

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -25,12 +26,17 @@ module Terappel
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
+    I18n.enforce_available_locales = false
 
     config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
       g.view_specs         false
       g.helper_specs       false
       g.controller_specs   false
       g.routing_specs      false
+      g.assets false
+      g.helpers false
     end
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<div class='has-error'>#{html_tag}</div>".html_safe }
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Terappel::Application.config.session_store :cookie_store, key: '_terappel_session'
+Rails.application.config.session_store :cookie_store, key: '_terappel_session'
