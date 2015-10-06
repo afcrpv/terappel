6282fcd00f9ff076de3cc9b6c3558a8bf6491e5d
Andrea D'Amico <andrea.damico@gmail.com>
2015-10-06 11:32:48 +0200

    adding production devise mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'www.terappel.fr' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.mandrillapp.com',
