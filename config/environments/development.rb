51e84c0c3693316f5d2a51690dcebef7b1578e5a
Andrea D'Amico <andrea.damico@gmail.com>
2016-01-15 14:22:11 +0100

    fix bullet and search path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
+  unless defined? Bullet
+    config.after_initialize do
+      Bullet.enable = true
+      Bullet.bullet_logger = true
+      Bullet.console = true
+      Bullet.rails_logger = true
+      Bullet.add_footer = true
+    end
   end
 end
