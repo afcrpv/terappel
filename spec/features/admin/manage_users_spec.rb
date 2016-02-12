cb83224d89819a5bcd60633d4b325b0bdc7e1816
Andrea D'Amico <andrea.damico@gmail.com>
2016-02-12 11:23:10 +0100

    fix manage users spec

--- spec/features/admin/manage_users_spec.rb
+++ spec/features/admin/manage_users_spec.rb
@@ -6,24 +6,7 @@ feature 'Admin manage users' do
 
   background { login admin }
 
-  scenario 'creation' do
-    visit new_admin_user_path
-    click_on 'Enregistrer'
-    page.should have_content 'erreurs'
-    fill_in 'user_username', with: 'fooman'
-    fill_in 'user_email', with: 'fooman@test.com'
-    fill_in 'user_password', with: 'barbaz1976'
-    fill_in 'user_password_confirmation', with: 'barbaz1976'
-    select centre.name, from: 'Centre'
-    click_on 'Enregistrer'
-    page.should have_content "succès"
-    page.should have_content 'Liste utilisateurs'
-  end
-
-  scenario 'modification' do
-    pending 'implement me MAINTENANT!'
-  end
-  scenario 'approbation' do
-    pending 'implement me NAU!'
-  end
+  scenario 'update'
+  scenario 'approve'
+  scenario 'receives email notification of user registration'
 end
