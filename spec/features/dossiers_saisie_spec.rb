0418ff7aeef3b7454c981869c1276d8644b05736
Andrea D'Amico <andrea.damico@gmail.com>
2016-03-03 18:34:06 +0100

    temp commit

--- spec/features/dossiers_saisie_spec.rb
+++ spec/features/dossiers_saisie_spec.rb
@@ -19,7 +19,7 @@ feature 'Dossiers saisie' do
       fill_in 'codedossier', with: 'LY1111001'
       click_button 'OK'
       expect(page).to have_content("Le dossier LY1111001 n'existe pas")
-      click_link("Clickez ici pour le créer")
+      click_on("Clickez ici pour le créer")
       expect(page.find('#dossier_code').value).to eq('LY1111001')
     end
 
@@ -40,9 +40,9 @@ feature 'Dossiers saisie' do
     select 'Oui', from: 'dossier_expo_terato'
     select 'Oui', from: 'dossier_a_relancer'
     click_on 'Oui'
-    click_link 'EXPOSITION'
+    click_on 'EXPOSITION'
     within '.tab-pane#expositions' do
-      click_link 'Ajouter une exposition'
+      click_on 'Ajouter une exposition'
       expect(find('.nested-fields')).to be_visible
       select2 produit.name, from: 'Exposition', search: true
       find_link('Valider').trigger('click')
@@ -73,52 +73,46 @@ feature 'Dossiers saisie' do
     expect(page).to have_content "Dossier LY2013001, modification effectuée avec succès."
   end
 
-  xcontext 'correspondants' do
-    scenario 'demandeur can be created/updated from dossier form', :js do
+  context 'correspondants', :js do
+    scenario 'demandeur can be created/updated from dossier form' do
       visit new_dossier_path
       within '#dossier_demandeur_id_field' do
-        click_link 'Ajout'
+        click_on 'Ajout'
       end
-      expect(find('#correspondant_modal')).to have_content('Nouveau correspondant')
+      expect(find('#mainModal')).to have_content('Nouveau Correspondant')
       fill_in 'correspondant_nom', with: 'test test'
       select 'spec1', from: 'Spécialité'
       fill_in 'Ville', with: 'Lyon'
       fill_in 'Code Postal', with: '69005'
-      within '.modal-footer' do
-        click_link 'Enregistrer'
-        sleep 2
-      end
+      click_on 'Créer un(e) Correspondant'
       expect(find('#dossier_demandeur_id_field')).to have_content('test test - spec1 - 69005 - Lyon')
       find('#dossier_demandeur_id_field').find_link('Voir/modifier').click
-      within '#correspondant_modal' do
+      within '#mainModal' do
         expect(page).to have_content('test test - spec1 - 69005 - Lyon')
       end
       fill_in 'correspondant_nom', with: 'test1 test'
-      click_link 'Enregistrer'
-      sleep 2
+      click_on 'Modifier ce(tte) Correspondant'
       expect(find('#dossier_demandeur_id_field')).to have_content 'test1 test - spec1 - 69005 - Lyon'
     end
 
-    context "for corr à relancer" do
+    context "for corr à relancer", :js do
       background do
         visit new_dossier_path
         within '#dossier_demandeur_id_field' do
-          click_link 'Ajout'
+          click_on 'Ajout'
         end
         sleep 2
         fill_in 'correspondant_nom', with: 'test test'
         select 'spec1', from: "Spécialité"
         fill_in 'Ville', with: 'Lyon'
         fill_in 'Code Postal', with: '69005'
-        within '.modal-footer' do
-          click_link 'Enregistrer'
-        end
+        click_on 'Créer un(e) Correspondant'
         sleep 2
         select 'Oui', from: 'Relance'
         sleep 1
       end
 
-      scenario "can copy assigned correspondant to corr à relancer", js: true do
+      scenario "can copy assigned correspondant to corr à relancer" do
         within '#relance' do
           click_button 'Oui'
         end
@@ -128,31 +122,25 @@ feature 'Dossiers saisie' do
         end
       end
 
-      scenario "can create new corr à relancer", js: true do
+      scenario "can create new corr à relancer" do
         within '#relance' do
           click_button 'Non'
-          sleep 4
+          sleep 2
         end
         within '#dossier_relance_id_field' do
-          click_link 'Ajout'
+          click_on 'Ajout'
         end
-        expect(find('#correspondant_modal')).to have_content('Nouveau correspondant')
+        expect(find('#mainModal')).to have_content('Nouveau Correspondant')
         fill_in 'correspondant_nom', with: 'test testone'
         select 'spec1', from: "Spécialité"
         fill_in 'Ville', with: 'Lyon'
         fill_in 'Code Postal', with: '69005'
-        within '.modal-footer' do
-          click_link 'Enregistrer'
-          sleep 4
-        end
+        click_on 'Créer un(e) Correspondant'
         expect(find('#dossier_relance_id_field')).to have_content('test testone - spec1 - 69005 - Lyon')
         find('#dossier_relance_id_field').find_link('Voir/modifier').click
-        expect(find('#correspondant_modal')).to have_content('test testone - spec1 - 69005 - Lyon')
+        expect(find('#mainModal')).to have_content('test testone - spec1 - 69005 - Lyon')
         fill_in 'correspondant_nom', with: 'test1 testone'
-        within '.modal-footer' do
-          click_link 'Enregistrer'
-          sleep 2
-        end
+        click_on 'Modifier ce(tte) Correspondant'
         expect(find('#dossier_relance_id_field')).to have_content('test1 testone - spec1 - 69005 - Lyon')
       end
     end
