792ff750992ca22e35b74a20b4ee9d33b0720ac3
Andrea D'Amico <andrea.damico@gmail.com>
2016-03-11 16:25:58 +0100

    unfreeze

--- app/decorators/dossiers_decorator.rb
+++ app/decorators/dossiers_decorator.rb
@@ -1,6 +1,6 @@
 require 'csv'
 class DossiersDecorator < Draper::CollectionDecorator
-  COLUMNS_FOR_CSV = [:code, :date_appel, :motif_code, :age, :poids, :taille, :atcds_fam, :atcds_perso, :grsant, :fcs, :geu, :miu, :ivg, :img, :nai, :tabac, :alcool, :age_grossesse, :evolution, :terme, :modaccouch, :nbr_bebes, :bb1_sexe, :bb1_poids, :bb1_taille, :bb1_pc, :bb1_apgar1, :bb1_apgar5, :malformation, :pathologie, :expo_terato].freeze
+  COLUMNS_FOR_CSV = [:code, :date_appel, :motif_code, :age, :poids, :taille, :atcds_fam, :atcds_perso, :grsant, :fcs, :geu, :miu, :ivg, :img, :nai, :tabac, :alcool, :age_grossesse, :evolution, :terme, :modaccouch, :nbr_bebes, :bb1_sexe, :bb1_poids, :bb1_taille, :bb1_pc, :bb1_apgar1, :bb1_apgar5, :malformation, :pathologie, :expo_terato]
 
   (1..3).each do |i|
     %w(produit indication dose expo_terme de a de2 a2).each do |name|
